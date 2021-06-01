add2bc2f1642fdf17c2fa26be200f6b8032de60e
pavan.k <kaparthi.pavan@gmail.com>
2021-06-01 10:16:14 +0530

    added new module tests

---
+++ src/test/java/tests/ContactUsTests.java
@@ -0,0 +1,13 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class ContactUsTests {
+	
+	@Test
+	public void verifyContactUsPage()
+	{
+		System.out.println("Steps to validate contact us page..");
+	}
+
+}
